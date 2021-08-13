ed96a1667afb82da92abefd926fe9bd0efc0bd83
Dragan Pepic <d.pepic@hotmail.com>
2021-08-13 16:26:01 +0200

    gjhg

--- Unos/Program.cs
+++ Unos/Program.cs
@@ -6,6 +6,15 @@ namespace Unos
     {
         static void Main(string[] args)
         {
+            int bbroj = 5;
+            string istoBroj = "5";
+            Console.WriteLine(bbroj + 3);
+            Console.WriteLine(istoBroj + 3);
+
+            Console.WriteLine("Upisi broj: ");
+            int broooj = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{broooj} plus 12 jeste {broooj + 12}");
+
             string unos;
             int broj = 1;
             do

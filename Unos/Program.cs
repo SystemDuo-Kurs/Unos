8db7313b3b66bd5d3eebaf023d20237b7eba4a45
Dragan Pepic <d.pepic@hotmail.com>
2021-08-13 15:15:56 +0200

    Add project files.

---
+++ Unos/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Unos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string unos;
+            int broj = 1;
+            do
+            {
+                broj++;
+                Console.WriteLine($"Broj je sada {broj}, da li zelite da nastavite?");
+                unos = Console.ReadLine();
+            } while (unos == "da");
+        }
+    }
+}
